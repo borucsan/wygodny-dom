b99209e1f468e903a03902dc591e34caca1ab2cb
Dawid Karwot <dawidkarwot@gmail.com>
2025-07-14 21:07:01 +0200

    Update layout breakpoints from lg to md and other improvements

--- pages/lead_done.vue
+++ pages/lead_done.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="flex flex-col justify-center !px-2 lg:pt-24">
-        <div class="flex justify-end lg:hidden pt-6 pb-3"><img class="" src="/assets/images/phones2.png"></div>
-        <h3 class="font-poppins text-xl tracking-tighter mb-12 text-center">Wyświetlimy kilka pytań od
+        <div class="flex justify-end md:hidden"><img class="" src="/assets/images/bg3.png"></div>
+        <h3 class="font-poppins text-xl tracking-tighter mb-12 text-center">Wyświetlimy kilka nieobowiązkowych pytań od
                 Partnerów Serwisu</h3>
         
         <div class="flex justify-center w-full my-12">
