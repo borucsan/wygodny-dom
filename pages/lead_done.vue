67b2393a37befd0295959e834e734760b7717a66
Aneta <a.mandera@salelifter.pl>
2026-04-27 14:57:37 +0200

    Update lead_done.vue
    Zmiana copy na "Już za chwilę rozpocznie się KONKURS. Przygotuj się! Najszybszy czas wygrywa."

--- pages/lead_done.vue
+++ pages/lead_done.vue
@@ -1,8 +1,7 @@
 <template>
     <section class="flex flex-col justify-center !px-2 lg:pt-24">
         <div class="flex justify-end md:hidden"><img class="" src="/assets/images/bg3.png"></div>
-        <h3 class="font-poppins text-xl tracking-tighter mb-12 text-center">Wyświetlimy kilka pytań od
-                Partnerów Serwisu</h3>
+        <h3 class="font-poppins text-xl tracking-tighter mb-12 text-center">Już za chwilę rozpocznie się KONKURS. Przygotuj się! Najszybszy czas wygrywa.</h3>
         
         <div class="flex justify-center w-full my-12">
             <UButton type="button" color="black" size="md" :disabled="selected" @click="navigateTo('/continue')">
